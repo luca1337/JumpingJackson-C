395d3635e9a849f05cc8d96a2ed875fc882387a3
luca1337 <biohazard911@hotmail.it>
2019-01-29 11:50:43 +0100

    fixed crash(?)

--- shaders/vertex.glsl
+++ shaders/vertex.glsl
@@ -11,9 +11,15 @@ uniform float depth = 1;
 
 out vec2 out_uv;
 
+float linear_convert(float value, float old_min, float old_max, float new_min, float new_max)
+{
+    float gradient = (value - old_min) / (old_max - old_min);
+    return new_min + gradient * (new_max - new_min);
+}
+
 void main()
 {
-    gl_Position = vec4(vec3(((vertex.x * width) + position.x)*(600.0/800.0), (vertex.y * height) + position.y, vertex.z), depth);
+    gl_Position = vec4(vec3(((vertex.x * width) + position.x)*(600.0/800.0) - 0.2, (vertex.y * height) + position.y, vertex.z), depth);
 
     out_uv = uvs;
 }
